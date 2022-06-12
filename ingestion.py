4691b9e9d9a25a91229773f1f25f3ad55e25ea9e
jonathan <jonathan@fli.co.id>
2022-06-12 11:11:51 +0700

    ganti connection

--- ingestion.py
+++ ingestion.py
@@ -1,11 +1,29 @@
-import psycopg2
+# import psycopg2
 
-conn    = psycopg2.connect(host='digitalskola7-do-user-7592846-0.b.db.ondigitalocean.com', dbname='deadpool', user='doadmin', password='AVNS_TDrSDUoeoSNF5rVVSLx', port='25061')
-curr     = conn.cursor()
+# conn    = psycopg2.connect(host='digitalskola7-do-user-7592846-0.b.db.ondigitalocean.com', dbname='deadpool', user='doadmin', password='AVNS_TDrSDUoeoSNF5rVVSLx', port='25061')
+# curr     = conn.cursor()
+# csv_file = open('D:\Jonathan\Digital Skola\Week 5\jonathan_products.csv' , 'w')
+# sql     = 'COPY (select * from products) TO STDOUT WITH CSV HEADER'
+# curr.copy_expert(sql , csv_file)
+# csv_file.close()
+# curr.close()
+# conn.close()
+import psycopg2
+import os
+
+SQL_USER = os.environ.get('OLTP_USERNAME')
+SQL_PASS = os.environ.get('OLTP_PASSWORD')
+SQL_HOST = os.environ.get('OLTP_HOST')
+SQL_PORT = os.environ.get('OLTP_PORT')
+SQL_DB = os.environ.get('OLTP_DATABASE')
+
+conn = psycopg2.connect(host=SQL_HOST, dbname=SQL_DB, user=SQL_USER, password=SQL_PASS, port=SQL_PORT)
+cur = conn.cursor()
+sql = 'COPY (select * from products) TO STDOUT WITH CSV HEADER'
 csv_file = open('D:\Jonathan\Digital Skola\Week 5\jonathan_products.csv' , 'w')
-sql     = 'COPY (select * from products) TO STDOUT WITH CSV HEADER'
-curr.copy_expert(sql , csv_file)
+cur.copy_expert(sql,csv_file)
 csv_file.close()
-curr.close()
-conn.close()
+cur.close
+conn.close
+
 
